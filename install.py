12764a4b61c891ebc3067c4f70a5babb96a177d6
Random-H <1092444593@qq.com>
2022-12-26 22:55:19 +0800

    git@github.com:Random-H/Termux.git

--- install.py
+++ install.py
@@ -14,12 +14,14 @@ os.system("mv $PREFIX/etc/php-fpm.d/www.conf.bak")
 os.system("cp conf/www.conf $PREFIX/etc/php-fpm.d/www.conf")
 #创建网页目录
 path = "/storage/emulated/0/www"
-os.mkdir(path)
+if  os.mkdir(path):
+    print("=======================================================\n")
+    print("请先执行termux-setup-storage,获取本机存储权限再执行python install.py\n")
+    print("=======================================================")
 if os.path.exists(path):
-    print("1.创建www文件夹成功，路径为%s"%path)
-    print("2.管理地址为127.0.0.1:8081")
-    print("3.将项目放入该目录，游览器输入127.0.0.1:8080即可")
-    print("4.首页一定要命名为index.*,如index.html index.php")
+    print("=======================================================\n")
+    print("1.创建www文件夹成功，路径为%s\n"%path)
+    print("=======================================================")
 else:
     print("创建www文件夹失败失败,请重试")
 
@@ -28,5 +30,10 @@ os.system("cp -r web $PREFIX/share/nginx/html/web")
 
 os.system("nginx")
 os.system("php-fpm")
+print("=======================================================\n")
+print("2.管理地址为127.0.0.1:8900")
+print("3.将项目放入该目录，游览器输入127.0.0.1:8080即可")
+print("4.首页一定要命名为index.*,如index.html index.php\n")
+print("=======================================================")
 
 
