c2eee4ddd25aa710dcaffdc77d22ce135ed14426
Random-H <1092444593@qq.com>
2022-12-28 18:36:14 +0800

    git@github.com:Random-H/Termux.git

--- install.py
+++ install.py
@@ -10,16 +10,16 @@ if ok == 'n':
 os.system("cd ~")
 #更新软件包
 os.system("rm $PREFIX/etc/apt/sources.list")
-os.system("cp conf/sources.list $PREFIX/etc/apt/sources.list")
+os.system("cp ~/Termux/conf/sources.list $PREFIX/etc/apt/sources.list")
 os.system("apt update && apt upgrade -y")
 #安装vim nginx php php-fpm
 os.system("pkg install vim nginx php php-fpm -y")
 #移植配置文件
 os.system("mv $PREFIX/etc/nginx/nginx.conf $PREFIX/etc/nginx/nginx.conf.bak")
-os.system("cp conf/nginx.conf $PREFIX/etc/nginx/nginx.conf")
-os.system("cp conf/default.conf $PREFIX/etc/nginx/default.conf")
+os.system("cp ~/Termux/conf/nginx.conf $PREFIX/etc/nginx/nginx.conf")
+os.system("cp ~/Termux/conf/default.conf $PREFIX/etc/nginx/default.conf")
 os.system("mv $PREFIX/etc/php-fpm.d/www.conf.bak")
-os.system("cp conf/www.conf $PREFIX/etc/php-fpm.d/www.conf")
+os.system("cp ~/Termux/conf/www.conf $PREFIX/etc/php-fpm.d/www.conf")
 #创建网页目录
 path = "/storage/emulated/0/www"
 if os.path.exists(path):
@@ -34,8 +34,8 @@ if os.path.exists(path):
 else:
     print("创建www文件夹失败失败,请重试")
 
-os.system("cp index.html $PREFIX/share/nginx/html/index.html")
-os.system("cp -r web $PREFIX/share/nginx/html/web")
+os.system("cp ~/Termux/index.html $PREFIX/share/nginx/html/index.html")
+os.system("cp -r ~/Termux/web $PREFIX/share/nginx/html/web")
 
 os.system("nginx")
 os.system("php-fpm")
